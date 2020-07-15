90f7ba77e08cda99b12ce9ba8ab6b58f7581a1d0
hobbyluv <princewillchime43@gmail.com>
2020-07-16 00:34:49 +0100

    Updates

---
+++ precache-manifest.fab8967a60009f9915549d8951488ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8519c1f76b5e6bb22cdaa10148da03",
+    "url": "/my-todo-app-with-react.js/index.html"
+  },
+  {
+    "revision": "68de20286a816f2ac546",
+    "url": "/my-todo-app-with-react.js/static/css/main.1aa44dc5.chunk.css"
+  },
+  {
+    "revision": "dac97d94e68ffd1d81b8",
+    "url": "/my-todo-app-with-react.js/static/js/2.770a27b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-todo-app-with-react.js/static/js/2.770a27b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68de20286a816f2ac546",
+    "url": "/my-todo-app-with-react.js/static/js/main.96a4ee07.chunk.js"
+  },
+  {
+    "revision": "292444b39cef13f8ddcd",
+    "url": "/my-todo-app-with-react.js/static/js/runtime-main.7e8245b1.js"
+  }
+]);
